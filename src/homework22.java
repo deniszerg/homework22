75e4826f8726aeb44233ed2c8921b886f04945ed
Denis Trischenko <deniszerg1@gmail.com>
2023-11-03 16:32:53 +0100

    upgread homework22

--- src/homework22.java
+++ src/homework22.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class homework22 {
   public static void main(String[] args) {
-    System.out.println("Hello world.");
+
+    public static void main (String[]args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("inputt natur num");
+      int n = scanner.nextInt();
+      if (n <= 0) {
+        System.out.println("you inputted not a natural num");
+        return;
+      }
+
+      int[] arr = new int[n];
+      Random random = new Random();
+      arr[0] = random.nextInt(10);
+
+      for (int i = 1; i < arr.length; i++) {
+        arr[i] = arr[i - 1] + random.nextInt(10);
+        System.out.print(arr[i] + " ");
+      }
+    }
   }
 }
